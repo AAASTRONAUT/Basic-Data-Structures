d706d4f787659a374a53ee6be34911242ae1a5b2
Shivansh Jain <shivanshjain1802@gmail.com>
2023-02-28 11:32:25 +0530

    added stack.h for tree.cpp

---
+++ Tree/stack.h
@@ -0,0 +1,59 @@
+//
+//  stack.h
+//  Tree
+//
+//  Created by Shivansh Jain on 22/02/23.
+//
+
+#ifndef stack_h
+#define stack_h
+
+#include <iostream>
+#include <stdio.h>
+#include "queue.h"
+
+using namespace std;
+
+class stack{
+private:
+    int size;
+    int top;
+    node ** s;
+public:
+    stack(int size){
+        this->size = size;
+        top = -1;
+        s = new node*[size];
+    }
+    void push(node* x);
+    node * pop();
+    bool isEmpty();
+};
+
+
+void stack::push(node * x){
+    if(top == size-1) printf("Stack Overflow!!\n");
+    else{
+        top++;
+        s[top] = x;
+    }
+}
+
+node * stack::pop(){
+    node * x = NULL;
+    if(top == -1) {
+        printf("Stack underflow!!\n");
+    }
+    else{
+        x = s[top];
+        top--;
+    }
+    return x;
+}
+
+bool stack::isEmpty(){
+    if(top == -1) return true;
+    else return false;
+}
+
+#endif /* stack_h */
